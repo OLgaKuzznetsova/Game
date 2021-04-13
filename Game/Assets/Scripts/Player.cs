ddfd0c26c26f01af6cadbf0035725ac9b07625a9
OLgaKuzznetsova <jk.kuznetsova@gmail.com>
2021-04-13 20:09:36 +0500

    1 level

--- Game/Assets/Scripts/Player.cs
+++ Game/Assets/Scripts/Player.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-     Rigidbody2D rb;
+    Rigidbody2D rb;
     public float speed;
+    public float jumpHeight;
+    public Transform groundCheck;
+    private bool isGrounded;
+    private Animator anim;
     
    // int currentHealthPoints; //текущее количество жизней
 
@@ -17,26 +21,49 @@ public class Player : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
        // currentHealthPoints = maxHealthPoints;
+       anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+        CheckGround();
+        if (Input.GetAxis("Horizontal") == 0 && (isGrounded))
+        {
+            anim.SetInteger("State", 1);
+        }
+        else
+        {
+            Flip();
+            if (isGrounded)
+                anim.SetInteger("State", 2);
+        }
     }
 
     void FixedUpdate()
     {
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
     }
 
-    /*void Flip()
+    void Flip()
     {
         if (Input.GetAxis("Horizontal") > 0)
             transform.localRotation = Quaternion.Euler(0, 0, 0);
         else if (Input.GetAxis("Horizontal") < 0)
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
             
-    }*/
+    }
+
+    void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.1f);
+        isGrounded = colliders.Length > 1;
+        if (!isGrounded)
+            anim.SetInteger("State", 3);
+    }
 
     /*public void RecountHealthPoints(int deltaHP)
     {
--- Game/Assets/Scripts/camera.cs
+++ Game/Assets/Scripts/camera.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class camera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float speed = 3f;
+
+    public Transform target;
+    
     void Start()
     {
-        
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 position = target.position;
+        position.z = transform.position.z;
+        transform.position = Vector3.Lerp(transform.position, position, speed);
     }
 }
